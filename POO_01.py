b6c39b2264f7a3835347949be9dd71849a79d2e9
Nicko3DF <britonicolas2509@gmail.com>
2025-12-24 02:50:38 -0300

    test 1
    prueba 1

---
+++ POO_01.py
@@ -0,0 +1,15 @@
+
+# Clase
+
+# En python es necesario que la 1ra letra sea mayus
+class Auto:
+    marca = ''
+    modelo = 0
+    Placa = ''
+
+# Ya esta la 1ra clase creada
+# Ahora hay que añador objetos
+
+# Asi le pone asignamos la clase de auto al objeto creado
+taxi = Auto()
+print(taxi.modelo)
